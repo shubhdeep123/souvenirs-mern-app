c1829df3bc270100731b6ac7523f029cd8a9311a
shubhdeep123 <shubhdeepsharma213@gmail.com>
2021-06-26 23:19:19 +0530

    created form to send posts data to server and implemented redux

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Post from "./Post/Post";
+import useStyles from "./styles";
+import { useSelector } from "react-redux";
+
+const Posts = () => {
+  const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+  console.log(posts);
+  return (
+    <>
+      Posts
+      <Post />
+    </>
+  );
+};
+
+export default Posts;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,20 @@
-import react from "react";
+import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 
-ReactDom.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+// this reducers is combined reducer
+import reducers from "./reducers";
+
+// store is created to store all states and middleware 'thunk' is applied
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
