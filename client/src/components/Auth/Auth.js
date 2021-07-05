cba07dfc28ce416e6232b95a9bdc98d1f43ae801
shubhdeep123 <shubhdeepsharma213@gmail.com>
2021-07-05 21:14:09 +0530

    Added authentication screen with google oAuth

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import {useHistory} from 'react-router-dom'
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+} from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import GoogleLogin from "react-google-login";
+import useStyles from "./styles";
+import Input from "./Input";
+import Icon from "./icon";
+import { AUTH } from "../../constants/authContants";
+
+const Auth = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const history = useHistory()
+
+  const handleSubmit = () => {};
+  const handleChange = () => {};
+  const handleShowPassword = () => setShowPassword((prevState) => !prevState);
+  const switchMode = () => {
+    setIsSignUp((prevState) => !prevState);
+    handleShowPassword(false);
+  };
+
+  const googleSuccess = async (res) => {
+    const result = res?.profileObj;
+    const token = res?.tokenId;
+
+    try {
+      dispatch({ type: AUTH, payload: { result, token } });
+      history.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const googleError = (err) => {
+    console.log(err);
+    console.log("Google Sign In was unsuccessful. Try Again Later");
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={10}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignUp ? "Sign Up" : "Sign In"}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignUp && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+                <Input
+                  name="lastName"
+                  label="Last Name"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email Address"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignUp && (
+              <Input
+                name="confirm password"
+                label="Confirm Password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            {isSignUp ? "Sign Up" : "Sign In"}
+          </Button>
+          <GoogleLogin
+            clientId="611630157778-i8lefrmmbk74hnmcqpa0curbpej27a30.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <Button
+                className={classes.googleButton}
+                color="primary"
+                fullWidth
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                startIcon={<Icon />}
+                variant="contained"
+              >
+                Google Sign In
+              </Button>
+            )}
+            onSuccess={googleSuccess}
+            onFailure={googleError}
+            cookiePolicy="single_host_origin"
+          />
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignUp
+                  ? "Already have an account? Sign In"
+                  : "Don't have an account? Sign In"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Auth;
---
+++ client/src/components/Auth/Input.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { TextField, Grid, InputAdornment, IconButton } from "@material-ui/core";
+import Visibilty from "@material-ui/icons/Visibility";
+import VisibiltyOff from "@material-ui/icons/VisibilityOff";
+
+const Input = ({
+  name,
+  handleChange,
+  half,
+  label,
+  autoFocus,
+  type,
+  handleShowPassword,
+}) => {
+  return (
+    <Grid item xs={12} sm={half ? 6 : 12}>
+      <TextField
+        name={name}
+        onChange={handleChange}
+        variant="outlined"
+        required
+        fullWidth
+        label={label}
+        autoFocus={autoFocus}
+        type={type}
+        InputProps={
+          name === "password"
+            ? {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleShowPassword}>
+                      {type === "password" ? <Visibilty /> : <VisibiltyOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }
+            : null
+        }
+      />
+    </Grid>
+  );
+};
+
+export default Input;
